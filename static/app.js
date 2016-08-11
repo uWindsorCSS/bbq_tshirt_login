a80f31be68ef864c970dc3c25d459e71bc88fb4a
Quinn Perfetto <thequinnftw@gmail.com>
2016-08-11 11:04:24 -0400

    added frontend logic for checkin

--- static/app.js
+++ static/app.js
@@ -2,8 +2,24 @@ function autoComplete(query, cb) {
   $.get("/autocomplete/" + query, cb);
 }
 
+function loginResponseHandler(data) {
+  var feedback = $("#feedback");
+  feedback.removeClass("alert alert-success alert-danger");
+  feedback.text(data.message);
+
+  if (data.success) {
+    feedback.addClass("alert alert-success");
+  } else {
+    feedback.addClass("alert alert-danger");
+  }
+}
+
 $(document).ready(function() {
   $("#full-name").typeahead({
     source: autoComplete
+  });
+
+  $("#login").on("click", function() {
+    $.get("/checkin/" + $("#full-name").val(), loginResponseHandler);
   });
 });
